65f0e152fdb37bb1e8af715c32c81c880e3e20fa
John Chavez <jrc2855@gmail.com>
2021-09-02 13:41:33 -0400

    Fixed app1

--- app1.js
+++ app1.js
@@ -1,5 +1,8 @@
 var button = document.querySelector('button');
 
 button.onclick = function panda(){
-    promptUser(["Pandas eat 26 to 84 pounds of Bamboo per day."]);
+    alert("Pandas eat between 26 and 84 pounds of Bamboo per day.") ;
 }
+
+
+button();
