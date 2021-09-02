b1a8e3d1b1a02c5dfc4330ab541848500013ec45
John Chavez <jrc2855@gmail.com>
2021-09-02 16:46:23 -0400

    app2 fix

--- app2.js
+++ app2.js
@@ -2,10 +2,10 @@
 let userAnswer = prompt("Do you like pandas?");
 console.log(userAnswer);
 if(userAnswer.toLowerCase() == 'no'){
-    document.write("img src=Panda.jpg");
+    document.write("<img src=Panda.jpg>");
     } else {
         (userAnswer.toLowerCase() == 'yes');
         {
-            document.write("img src=Panda2.jpg");
+            document.write("<img src=Panda2.jpg>");
         }
 }
