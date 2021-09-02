d6c7385f1d655aba64c78fdfcc464ea8e37574e2
John Chavez <jrc2855@gmail.com>
2021-09-02 16:09:42 -0400

    app2 fix

--- app2.js
+++ app2.js
@@ -1,15 +1,11 @@
 // This is my function for a user prompted question
-function Question(){
-    let userAnswer = prompt("Do you like pandas?");
-    console.log(userAnswer);
-    if(userAnswer() == 'no'){
-        document.write("img src=Panda.jpg");
+let userAnswer = prompt("Do you like pandas?");
+console.log(userAnswer);
+if(userAnswer() == 'no'){
+    document.write("img src=Panda.jpg");
     } else {
         (userAnswer() == 'yes');
         {
             document.write("img src=Panda2.jpg");
         }
-    }
 }
-
-Question ();
