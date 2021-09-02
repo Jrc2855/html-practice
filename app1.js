2f822a67383d35b99db57de5eab04ab8572ccba9
John Chavez <jrc2855@gmail.com>
2021-09-02 19:02:01 -0400

    app consolidation

--- app1.js
+++ app1.js
@@ -1,7 +1,30 @@
-var button = document.querySelector('button');
-document.getElementById("panda.emoji").style.cursor = "pointer";
-button.onclick = function panda(){
-    alert("Pandas eat between 26 and 84 pounds of Bamboo per day.") ;
+// This is my button function
+
+function button(){
+    var button = document.querySelector('button');
+    document.getElementById("panda.emoji").style.cursor = "pointer";
+    button.onclick = function panda(){
+        alert("Pandas eat between 26 and 84 pounds of Bamboo per day.") ;
+    }
 }
 
-button();
+// This is my function for a user prompted question
+function question(){
+
+    let userAnswer = prompt("Do you like pandas?");
+    console.log(userAnswer);
+    if(userAnswer.toLowerCase() == 'no'){
+        document.write("<img src=Panda.jpg>");
+        } else {
+            (userAnswer.toLowerCase() == 'yes');
+         {
+                document.write("<img src=Panda2.jpg>");
+        }
+    }
+}
+
+
+//This is my cursor change function
+function cursor() {
+    document.getElementById("clickme", "pandaemoji").style.cursor = "pointer";
+}
--- app3.js
+++
@@ -1,5 +0,0 @@
-function cursor() {
-    document.getElementById("clickme").style.cursor = "pointer";
-}
-
-cursor();
